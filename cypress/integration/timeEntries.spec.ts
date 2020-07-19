d02fa14737677cbcd5177b8521725babcf5dfbcb
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2020-07-19 11:05:48 +1000

    test: Specify user can add a time entry

--- cypress/integration/timeEntries.spec.ts
+++ cypress/integration/timeEntries.spec.ts
@@ -1,9 +1,9 @@
 import { addTimeEntry } from '../../src/features/timeentries/timeEntriesSlice'
 
 describe('Time Wimey', () => {
-  it('Can display time entries', () => {
-    cy.visit('/')
+  beforeEach(() => cy.visit('/'))
 
+  it('Can display time entries', () => {
     cy.get('[id^=\'timeEntry_\']').should('have.length', 2)
 
     cy.get('#timeEntry_0 input[id^=\'start\']').should('have.value', '09:00 AM')
@@ -20,8 +20,6 @@ describe('Time Wimey', () => {
   })
 
   it('Can edit time entries', () => {
-    cy.visit('/')
-
     cy.get('#timeEntry_0 input[id^=\'start\']').clear().type('09:30 AM').should('have.value', '09:30 AM')
     cy.get('#timeEntry_0 input[id^=\'end\']').clear().type('09:45 AM').should('have.value', '09:45 AM')
     cy.get('#timeEntry_0 input[id^=\'ticket\']').clear().type('ABC-123').should('have.value', 'ABC-123')
@@ -31,5 +29,18 @@ describe('Time Wimey', () => {
     cy.get('#timeEntry_1 input[id^=\'end\']').clear().type('12:00 PM').should('have.value', '12:00 PM')
     cy.get('#timeEntry_1 input[id^=\'ticket\']').clear().type('ZYX-321').should('have.value', 'ZYX-321')
     cy.get('#timeEntry_1 input[id^=\'details\']').clear().type('Exploited bananas').should('have.value', 'Exploited bananas')
+  })
+
+  it('Can add a time entry', () => {
+    cy.get('[id^=\'timeEntry_\']').as('timeEntries').should('have.length', 2)
+
+    cy.contains('+ New Entry').click()
+
+    cy.get('@timeEntries').should('have.length', 3)
+
+    cy.get('#timeEntry_2 input[id^=\'start\']').should('have.value', '03:15 PM')
+    cy.get('#timeEntry_2 input[id^=\'end\']').should('have.value', '')
+    cy.get('#timeEntry_2 input[id^=\'ticket\']').should('have.value', '')
+    cy.get('#timeEntry_2 input[id^=\'details\']').should('have.value', '')
   })
 })
