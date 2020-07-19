aaa9bbc3045c31588c9119c6587737a0146c9d11
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2020-07-19 15:10:43 +1000

    feat: Add NewTimeEntryButton

--- src/app/pages/Home.tsx
+++ src/app/pages/Home.tsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import TimeEntriesList from '../../features/timeentries/TimeEntriesList'
+import NewTimeEntryButton from '../../features/timeentries/NewTimeEntryButton'
 
-const Home = () => <TimeEntriesList />
+const Home = () => (
+  <>
+    <TimeEntriesList />
+    <NewTimeEntryButton />
+  </>
+)
 
 export default Home
