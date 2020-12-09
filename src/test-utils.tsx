895159138a76df635f420ada7f90830b56abda16
Braydon Hall <40751395+nobrayner@users.noreply.github.com>
2020-12-09 13:43:25 +1100

    feat: Add TimeCard builder

--- src/test-utils.tsx
+++ src/test-utils.tsx
@@ -3,6 +3,8 @@ import { createStore, EnhancedStore } from "@reduxjs/toolkit"
 import { render as rtlRender, RenderOptions as RTLRenderOptions, screen } from '@testing-library/react'
 import { Provider } from "react-redux"
 import { RootState, reducer } from "./store"
+import { build, fake, perBuild, sequence } from '@jackfranklin/test-data-bot'
+import { TimeCard } from './types'
 
 interface RenderOptions extends RTLRenderOptions {
   initialState?: RootState
@@ -14,3 +16,18 @@ export function render(ui: React.ReactElement, {initialState, store = createStor
 }
 
 export { screen } from '@testing-library/react'
+
+export const timeCardBuilder = build<TimeCard>('TimeCard', {
+  fields: {
+    id: sequence(),
+    start: fake(f => f.date.recent(1)),
+    end: fake(f => f.date.recent(1)),
+    ticket: fake(f => `${f.lorem.word()}-${f.random.number()}`),
+    details: fake(f => f.lorem.words()),
+  },
+  traits: {
+    noend: {
+      overrides: { end: perBuild(() => undefined) }
+    }
+  }
+})
